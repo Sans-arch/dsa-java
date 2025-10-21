6ca366cd5d4cc03792a574bc87d8d2708aa6d70c
Sans-arch <sansfleitas@gmail.com>
2025-10-21 14:36:56 -0300

    write removelast method of dll

--- src/datastructures/doubly_linked_list/Main.java
+++ src/datastructures/doubly_linked_list/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
         myDLL.append(10);
+        myDLL.append(15);
+
+        myDLL.removeLast();
 
         myDLL.printList();
     }
