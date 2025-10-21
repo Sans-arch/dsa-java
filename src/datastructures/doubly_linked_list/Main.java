2b4ff9925ae942296bf7fff973b085f5bca437b5
Sans-arch <sansfleitas@gmail.com>
2025-10-21 15:40:57 -0300

    write removefirst method

--- src/datastructures/doubly_linked_list/Main.java
+++ src/datastructures/doubly_linked_list/Main.java
@@ -3,9 +3,15 @@ package datastructures.doubly_linked_list;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
-        myDLL.removeLast();
-        myDLL.prepend(10);
-        myDLL.prepend(20);
+        myDLL.append(14);
+        myDLL.append(18);
+        myDLL.append(59);
+
+        myDLL.removeFirst();
+        myDLL.removeFirst();
+        myDLL.removeFirst();
+        myDLL.removeFirst();
+        myDLL.removeFirst();
 
         myDLL.printList();
     }
